68c596104ae2c63c84c59ba8f61cf127a5be3083
abawchen <abaw.chen@gmail.com>
2018-04-10 15:55:59 +0800

    feat: Add zsh.

---
+++ zsh/.my.sh
@@ -0,0 +1,9 @@
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+
+export NVM_DIR="/Users/ylchen/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# https://github.com/creationix/nvm#installation
+source ~/.nvm/nvm.sh
+
